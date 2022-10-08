104cee850f94f28babd1b04ff46ac175de51be05
Mostafa <Mostafa0Mohamed2002@gmail.com>
2022-10-08 05:02:31 +0200

    add functionality for start button

--- app/src/main/java/com/mostafadev/quiz_app/MainActivity.kt
+++ app/src/main/java/com/mostafadev/quiz_app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.mostafadev.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        start_btn.setOnClickListener {
+            val usernametext=edittext_username.text.toString()
+            if(usernametext.isNotEmpty()){
+                Toast.makeText(this, usernametext,Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
