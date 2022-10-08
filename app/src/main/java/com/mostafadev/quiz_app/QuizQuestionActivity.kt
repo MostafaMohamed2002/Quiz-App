05b064695ba86f32ead545a6e7855c504e7f221d
Mostafa <Mostafa0Mohamed2002@gmail.com>
2022-10-08 06:47:37 +0200

    add good font and test it

--- app/src/main/java/com/mostafadev/quiz_app/QuizQuestionActivity.kt
+++ app/src/main/java/com/mostafadev/quiz_app/QuizQuestionActivity.kt
@@ -2,10 +2,21 @@ package com.mostafadev.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_quiz_question.*
 
 class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+        val currentPosistion =1
+        progress_circle.progress=currentPosistion
+        progress_tv.text="$currentPosistion"
+        val questionList= getQuestions()
+        val question=questionList[currentPosistion-1]
+        question_tv.text=question.question
+        option_one.text=question.optionOne
+        option_two.text=question.optionTwo
+        option_three.text=question.optionThree
+        option_four.text=question.optionFour
     }
 }
